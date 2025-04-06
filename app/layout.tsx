23bb124edf96d9d218e6888c44983a0f1d86d481
bouzangad zakaria <bouzangadzakaria@gmail.com>
2025-04-06 15:20:54 +0100

    chore: update dependencies and add new packages for dropdown menu and theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
@@ -24,7 +25,12 @@ export default function RootLayout({
       <body
         className={cn('min-h-screen bg-dark-300 font-sans antialiased',fontSans.variable)}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="Dark"
+          >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl underline">
         home
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
