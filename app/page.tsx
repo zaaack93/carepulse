27ab2c6987fd23f2a92a6ecb63846bf43404f2db
bouzangad zakaria <bouzangadzakaria@gmail.com>
2025-04-06 15:47:17 +0100

    working on page home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">
-        home
-      </h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO : OTP VERIFICATION */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+           src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; {new Date().getFullYear()} CarePulse
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+              
+          </div>
+
+        </div>
+
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
